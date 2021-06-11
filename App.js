feb27cf0ec9c64e81d2637dea195e408d447b4d2
J.S <pachipico1@gmail.com>
2021-06-11 20:38:57 +0900

    Complete quiz app

--- App.js
+++ App.js
@@ -6,8 +6,10 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import styled from 'styled-components';
+import movieList from './movieList';
+import _ from 'lodash';
 
 const Container = styled.SafeAreaView`
   flex: 1;
@@ -32,17 +34,51 @@ const Label = styled.Text`
   font-size: 18px;
 `;
 
+function getInitials(string) {
+  return string
+    .split('')
+    .map(char => {
+      const index = (char.charCodeAt(0) - 44032) / 28 / 21;
+      if (index >= 0) return String.fromCharCode(index + 4352);
+      return char;
+    })
+    .join('');
+}
+
 const App: () => Node = () => {
+  const [quizList, setQuizList] = useState(_.shuffle(movieList));
+  const [mode, setMode] = useState('quiz');
+  const onPress = useCallback(() => {
+    if (mode === 'answer') {
+      setQuizList(quizList.slice(1));
+    }
+    setMode(mode === 'quiz' ? 'answer' : 'quiz');
+  }, [mode]);
+  const retry = useCallback(() => {
+    setQuizList(_.shuffle(movieList));
+    setMode('quiz');
+  }, [quizList]);
   return (
     <>
       <Container>
         <Content>
-          <Quiz>ㅂㄷ</Quiz>
+          {quizList.length ? (
+            <Quiz>
+              {mode === 'quiz' ? getInitials(quizList[0]) : quizList[0]}
+            </Quiz>
+          ) : (
+            <Quiz>끝</Quiz>
+          )}
         </Content>
-
-        <Button>
-          <Label>확인</Label>
-        </Button>
+        {quizList.length ? (
+          <Button onPress={onPress}>
+            <Label>{mode === 'quiz' ? '정답 확인' : '다음'}</Label>
+          </Button>
+        ) : (
+          <Button onPress={retry}>
+            <Label>retry</Label>
+          </Button>
+        )}
       </Container>
     </>
   );
